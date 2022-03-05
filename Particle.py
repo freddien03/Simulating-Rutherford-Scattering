dc951effb6972bf90c3a19a7946f545ba96f980d
freddien03 <60051731+freddien03@users.noreply.github.com>
2022-03-05 19:22:45 +0000

    Completed intro and started overview

--- Particle.py
+++ Particle.py
@@ -1,6 +1,9 @@
+#Temporary file for mockup of Particle class
+
 import numpy as np
 
 class Particle:
+    E0 = 8.85e-12
 
     def __init__(self, mass: float, charge: float, position: np.array, velocity: np.array):
         self.mass = mass
@@ -10,3 +13,6 @@ class Particle:
 
     def distance(self, other):
          return (((self.position[0]-other.position[0])**2)+((self.position[1]-other.position[1])**2))**0.5
+
+    def attraction(self, other):
+        return (self.charge*other.charge)/(4*np.pi*self.E0*(self.distance(other)**2))
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Simulating-Rutherford-Scattering
+
+## 1. Introduction
+
+### 1.1 history of RS
+
+## 2. Model Overview
+
+### 2.1 Aims
+
+## 2.2 What to consider
+
+### 2.2.1 Materials
+
+### 2.2.2 Alpha particle velocity
+
+### 2.2.3
